da573035e2300b00ab98bc61d46bc9b34c140674
Daniel G <dgimenez123@gmail.com>
2023-07-19 13:51:29 +0200

    intento deploy

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,11 @@
+
+import abouts from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import skills from './skills'
+import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from "./works"
+import experiences from "./experiences"
+
+export const schemaTypes = [abouts, brands, contact, skills, testimonials, workExperience,experiences, works]
